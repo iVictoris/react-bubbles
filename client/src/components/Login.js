bd95c643d3a874be32674213bacdbde662c53f5a
Victor <victor.tran@tempestas.io>
2020-05-03 21:20:13 -0400

    build login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post(
+        'http://localhost:5000/api/login',
+        data,
+      );
+      localStorage.setItem('token', response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="username"
+          ref={register({
+            required: true,
+          })}
+          defaultValue="Lambda School"
+        />
+        <input
+          type="password"
+          name="password"
+          defaultValue="i<3Lambd4"
+          ref={register({
+            required: true,
+          })}
+        />
+
+        <input type="submit" />
+      </form>
     </>
   );
 };
